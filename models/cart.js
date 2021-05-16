199c63dfd94af03ea4cf5f660be7e8b57423d3c3
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-05-17 00:28:30 +0200

    init: Added Cart model

---
+++ models/cart.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const CartSchema = new Schema({
+    totalprice: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+}, {
+    versionKey: false,
+    collection: "cart",
+});
+
+const cart = mongoose.Model("Cart", CartSchema);
+
+module.exports = cart;
