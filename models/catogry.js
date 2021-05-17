5b43b2202dcffb52db66845660fc77154a378cd7
abraryahia <abraryahya18@gmail.com>
2021-05-17 11:42:09 +0200

    init : Added order , orderItem , catogry and review model

---
+++ models/catogry.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const CatogrySchema = new Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+    },
+    description: {
+        type: String,
+        maxlength:400,
+    }
+}, {
+    versionKey: false,
+    collection: "catogries",
+});
+
+const catogry = mongoose.Model("Catogry", CatogrySchema);
+
+module.exports = catogry;
