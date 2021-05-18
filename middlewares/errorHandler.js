357037da3d22f3f9ddfd4ce411964ad19b68eec3
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-05-19 01:45:22 +0200

    fix: Add Optional Error Status Code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const customError = (errorString, errorCode) => {
     const error = new Error(errorString);
-    error.statusCode = errorCode;
+    error.statusCode = errorCode || 500;
     throw error;
 }
 module.exports = customError;
