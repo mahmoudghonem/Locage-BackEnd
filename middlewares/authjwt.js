0e9344d1eb9a6af84f2725bde21d899e8f60dd7b
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-05-22 00:16:47 +0200

    fix: Unhandled Rejection at: Promise

--- middlewares/authjwt.js
+++ middlewares/authjwt.js
@@ -3,20 +3,24 @@ const jwt = require('jsonwebtoken');
 const CustomError = require('../functions/errorHandler');
 
 const auth = async (req, res, next) => {
-    const { authorization } = req.headers
-    if (!authorization)
-        return new CustomError('AUTHORIZATION_HEADER_REQUIRED', 401);
     try {
-        const token = authorization.split(" ")[1];
-        let decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
-        if (!decodedToken)
-            return new CustomError('NOT_AUTHORIZATION', 401);
+        const { authorization } = req.headers
+        if (!authorization)
+            new CustomError('AUTHORIZATION_HEADER_REQUIRED', 401);
+        try {
+            const token = authorization.split(" ")[1];
+            let decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
+            if (!decodedToken)
+                new CustomError('NOT_AUTHORIZED', 401);
 
-        const { id } = decodedToken;
-        req.userId = id;
-        next();
+            const { id } = decodedToken;
+            req.userId = id;
+            next();
+        } catch (e) {
+            new CustomError('NOT_AUTHORIZED', 401);
+        }
     } catch (e) {
-        next((new CustomError('NOT_AUTHORIZED', 401)));
+        next(e)
     }
 }
 module.exports = auth;
