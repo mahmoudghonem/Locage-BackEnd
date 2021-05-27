a14c61b8c92b64ee9d0a62f311fa3e9bc99cc66c
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-05-27 03:42:45 +0200

    feat: Added retrieve, edit and delete product

--- routes/product.js
+++ routes/product.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const { add } = require('../services/product');
+const { add, getProducts, getProduct, edit, remove } = require('../services/product');
+const upload = require('../functions/multipleImageUpload');
+
 
 // Product Routes
 router.get('/', retrieveProducts);
-router.post('/add-product', addProduct);
+router.get('/:id', retrieveProduct);
+router.post('/add', upload, addProduct);
+router.patch('/edit/:id', editProduct);
+router.delete('/delete/:id', deleteProduct);
 
 
 /* Routes Handlers */
 // Retrieve all products
-function retrieveProducts(req, res){
-    res.json("Here it comes..");
+function retrieveProducts(req, res, next){
+    getProducts().then(result => res.json(result))
+    .catch(error => next(error))
+}
+
+// Retrieve a product by id
+function retrieveProduct(req, res, next){
+    const { id } = req.params;
+    getProduct(id).then(result => res.json(result))
+    .catch(error => next(error))
 }
 
 // Add a new product
 function addProduct(req, res, next){
-    add(req, res).then(result => res.json(result))
+    const { body, files } = req;
+    add(body, files).then(result => res.json({message: "Product has been added.", result: result}))
+    .catch(error => next(error));
+}
+
+// Modify an existing product 
+function editProduct(req, res, next){
+    const { body } = req;
+    const { id } = req.params;
+    edit(body, id).then(result => res.json({message: "Product has been edited.", result: result}))
+    .catch(error => next(error));
+}
+
+// Remove a product 
+function deleteProduct(req, res, next){
+    const { id } = req.params;
+    remove(id).then(result => res.json({message: "Product has been deleted", result: result}))
     .catch(error => next(error));
 }
 
--- services/product.js
+++ services/product.js
@@ -1,12 +1,62 @@
+const customError = require('../functions/errorHandler');
 const Product = require('../models/product');
 
-const add = async (req, res) => {
-    const newProduct = new Product(req.body);
-    await newProduct.save()
-    .then(result => res.json(result))
-    .catch(error => res.status(400).json(error));
+
+// Check that id is correct and castable
+const checkId = (id) => {
+    if(id.length > 24 || id.length < 24) customError("UNCASTABLE_OBJECTID", 400)
+}
+
+// Check that the body of the request is not empty
+const isEmpty = (obj) => {
+    if(Object.keys(obj).length === 0 && obj.constructor === Object) customError("BAD_REQUEST", 400);
+}
+
+const getProducts = async () => {
+    // pagination
+    return await Product.find();
+}
+
+const getProduct = async (id) => {
+    checkId(id);
+    return await Product.findById(id);
+}
+
+const add = async (product, files) => {
+    const photos = [];
+    isEmpty(product);
+    if(files.length !== 0)
+        files.forEach(file => photos.push(file.filename));
+    const newProduct = new Product({...product, photos: photos});
+    return await newProduct.save();
+}
+
+const edit = async (editedData, id) => {
+    // check 
+    checkId(id);
+    
+    const productToEdit = await Product.findById(id);
+
+    // check that product exists
+    if(!productToEdit) customError("PRODUCT_NOT_FOUND", 404);
+
+    // check that editedData is not empty
+    isEmpty(editedData);
+
+    return await Product.findByIdAndUpdate(id, editedData, {new: true});
+}
+
+const remove = async (id) => {
+    checkId(id);
+    const productToDelete = await Product.findById(id);
+    if(!productToDelete) customError("PRODUCT_NOT_FOUND", 404);
+    return await Product.findByIdAndDelete(id);
 }
 
 module.exports = {
-    add
+    add,
+    getProducts,
+    getProduct,
+    edit,
+    remove
 }
