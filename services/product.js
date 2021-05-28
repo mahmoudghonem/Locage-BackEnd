d601166ee9ee64da8e1e606c3512da3e63ce4c04
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-05-28 23:09:47 +0200

    feat: adding pm2 process manager for heroku

--- services/product.js
+++ services/product.js
@@ -13,64 +13,64 @@ const getProducts = async (req) => {
     const options = {
         limit: limit,
         page: page
-    }
+    };
     return await Product.paginate({}, options);
-}
+};
 
 const getProduct = async (id) => {
     checkId(id);
     const product = await Product.findById(id);
-    if(!product) customError("PRODUCT_NOT_FOUND", 404);
+    if (!product) customError("PRODUCT_NOT_FOUND", 404);
     return product;
-}
+};
 
 const add = async (product, files, userId) => {
     // check
-    const existingProduct = await Product.findOne({ title: product.title })
-    if(existingProduct) customError("TITLE_MUST_BE_UNIQUE", 400);
+    const existingProduct = await Product.findOne({ title: product.title });
+    if (existingProduct) customError("TITLE_MUST_BE_UNIQUE", 400);
 
     const loggedUser = await User.findById(userId);
-    if(!loggedUser) customError("UNAUTHORIZED", 401);
-    if(loggedUser.role != "vendor") customError("UNAUTHORIZED", 401); 
+    if (!loggedUser) customError("UNAUTHORIZED", 401);
+    if (loggedUser.role != "vendor") customError("UNAUTHORIZED", 401);
 
     const photos = [];
     const photosPublicId = [];
-    if(files.length !== 0){
-        for(const file of files){
+    if (files.length !== 0) {
+        for (const file of files) {
             const { path } = file;
             const result = await cloudinary.uploader.upload(path);
             photos.push(result.secure_url);
             photosPublicId.push(result.public_id);
         }
     }
-    
+
     isEmpty(product);
-    const newProduct = new Product({...product, photos: photos, photosPublicId: photosPublicId});
+    const newProduct = new Product({ ...product, photos: photos, photosPublicId: photosPublicId });
     return await newProduct.save();
-}
+};
 
 const edit = async (editedData, id, files, userId) => {
     // check 
     checkId(id);
-    
-    const existingProduct = await Product.findOne({ title: editedData.title })
-    if(existingProduct) customError("TITLE_MUST_BE_UNIQUE", 400);
+
+    const existingProduct = await Product.findOne({ title: editedData.title });
+    if (existingProduct) customError("TITLE_MUST_BE_UNIQUE", 400);
 
     const loggedUser = await User.findById(userId);
-    if(!loggedUser) customError("UNAUTHORIZED", 401);
-    if(loggedUser.role != "vendor") customError("UNAUTHORIZED", 401);
-    
+    if (!loggedUser) customError("UNAUTHORIZED", 401);
+    if (loggedUser.role != "vendor") customError("UNAUTHORIZED", 401);
+
     // check that product exists
     const productToEdit = await Product.findById(id);
-    if(!productToEdit) customError("PRODUCT_NOT_FOUND", 404);
+    if (!productToEdit) customError("PRODUCT_NOT_FOUND", 404);
 
     // check that editedData is not empty
     isEmpty(editedData);
 
     const photos = productToEdit.photos;
     const photosPublicId = productToEdit.photosPublicId;
-    if(files.length !== 0){
-        for(const file of files){
+    if (files.length !== 0) {
+        for (const file of files) {
             const { path } = file;
             const result = await cloudinary.uploader.upload(path);
             photos.push(result.secure_url);
@@ -78,26 +78,26 @@ const edit = async (editedData, id, files, userId) => {
         }
     }
 
-    return await Product.findByIdAndUpdate(id, {...editedData, photos: photos, photosPublicId: photosPublicId}, {new: true});
-}
+    return await Product.findByIdAndUpdate(id, { ...editedData, photos: photos, photosPublicId: photosPublicId }, { new: true });
+};
 
 const remove = async (id, userId) => {
     // check
     checkId(id);
 
     const loggedUser = await User.findById(userId);
-    if(!loggedUser) customError("UNAUTHORIZED", 401);
-    if(loggedUser.role != "vendor") customError("UNAUTHORIZED", 401);
+    if (!loggedUser) customError("UNAUTHORIZED", 401);
+    if (loggedUser.role != "vendor") customError("UNAUTHORIZED", 401);
 
     const productToDelete = await Product.findById(id);
-    if(!productToDelete) customError("PRODUCT_NOT_FOUND", 404);
+    if (!productToDelete) customError("PRODUCT_NOT_FOUND", 404);
 
     const { photosPublicId } = productToDelete;
 
-    photosPublicId.forEach(async(id) => await cloudinary.uploader.destroy(id, function(result) { console.log(result) }));
+    photosPublicId.forEach(async (id) => await cloudinary.uploader.destroy(id, function (result) { console.log(result) }));
 
     return await Product.findByIdAndDelete(id);
-}
+};
 
 module.exports = {
     add,
@@ -105,4 +105,4 @@ module.exports = {
     getProduct,
     edit,
     remove
-}
+};
