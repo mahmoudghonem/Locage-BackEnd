3c8243f11f27db689c1c584f43f42b8e3c8674f0
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-06-01 10:07:42 +0200

    init: Added subcategory router

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,15 @@ const storeRouter = require('./store');
 const productRouter = require('./product');
 const paymentRouter = require('./payment');
 const categoryRouter = require('./category');
+const subcategoryRouter = require('./subcategory');
 
 router.use('/users', userRouter);
 router.use('/stores', storeRouter);
 router.use('/products', productRouter);
 router.use('/payments', paymentRouter);
 router.use('/category', categoryRouter);
+router.use('/subcategory', subcategoryRouter);
+
 
 //set not found router middleware
 router.use((req, res, next) => {
