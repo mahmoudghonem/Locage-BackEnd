b1565bcd83f89bce4f5a7a770daa6410abafff1a
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-06-02 01:33:38 +0200

    feat: subcategory update & delete

--- routes/subcategory.js
+++ routes/subcategory.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { getProductsOfSubcategory } = require('../services/subcategory');
+const { getProductsOfSubcategory, editSubcategory, deleteSubcategory } = require('../services/subcategory');
+const authjwt = require("../middlewares/authjwt");
 
 
 router.get('/:id/products', retrieveProductsOfSubcategory);
+
+router.route('/:id')
+    .patch(authjwt, modifySubcategory)
+    .delete(authjwt, removeSubcategory)
 
 
 function retrieveProductsOfSubcategory(req, res, next){
     const { id: subcategoryId } = req.params;
     const { page, limit } = req.query;
     getProductsOfSubcategory(subcategoryId, page, limit).then(result => res.json({result: result}))
+    .catch(error => next(error));
+}
+
+function modifySubcategory(req, res, next){
+    const { id: subcategoryId } = req.params;
+    const { body: editedSubcategory, userId } = req;
+    editSubcategory(editedSubcategory, subcategoryId, userId).then(result => {
+        res.json({message: "Subcategory has been edited.", result: result});
+    })
+    .catch(error => next(error));
+}
+
+function removeSubcategory(req, res, next){
+    const { id: subcategoryId } = req.params;
+    const { userId } = req;
+    deleteSubcategory(subcategoryId, userId).then(result => {
+        res.json({message: "Subcategory has been deleted.", result: result});
+    })
     .catch(error => next(error));
 }
 
--- services/subcategory.js
+++ services/subcategory.js
@@ -1,6 +1,14 @@
 const Subcategory = require('../models/subcategory');
 const Product = require('../models/product');
+const User = require('../models/user');
 const customError = require('../functions/errorHandler');
+
+
+const loggedUserCheck = async (userId) => {
+    const loggedUser = await User.findById(userId);
+    if(!loggedUser) customError("UNAUTHORIZED_NOTLOGGEDIN", 401);
+    if(loggedUser.role !== "admin") customError("UNAUTHORIZED", 401);
+}
 
 
 const getProductsOfSubcategory = async (subcategoryId, page, limit) => {
@@ -21,6 +29,34 @@ const getProductsOfSubcategory = async (subcategoryId, page, limit) => {
     }
 }
 
+const editSubcategory = async (editedSubcategory, subcategoryId, userId) => {
+    // checks
+    await loggedUserCheck (userId);
+    if(!(await Subcategory.findById(subcategoryId))) customError("SUBCATEGORY_NOTFOUND", 404);
+
+    try{
+        return await Subcategory.findByIdAndUpdate(subcategoryId, editedSubcategory, { new: true });
+    } catch(error){
+        return customError(error.toString(), 500);
+    }
+}
+
+const deleteSubcategory = async (subcategoryId, userId) => {
+    // checks
+    await loggedUserCheck (userId);
+
+    try{
+        // delete products related to subcategory
+        await Product.deleteMany({ subcategoryId: subcategoryId });
+
+        return await Subcategory.findByIdAndDelete(subcategoryId);
+    } catch(error){
+        return customError(error.toString(), 500);
+    }
+}
+
 module.exports = {
-    getProductsOfSubcategory
+    getProductsOfSubcategory,
+    editSubcategory,
+    deleteSubcategory
 }
