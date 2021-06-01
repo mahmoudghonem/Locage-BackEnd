7e933addf24756865daa409cc2add6eaa04922b8
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-06-01 05:57:14 +0200

    feat: Create & retrieve categories

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const { retrieveAllCategories } = require('../services/category');
+const { retrieveAllCategories, createCategory } = require('../services/category');
+const authjwt = require("../middlewares/authjwt");
+
 
 router.route('/')
-    .get(retrieveAllCategories)
+    .get(getCategories)
+    .post(authjwt, createnewCategory);
+
+// router.route('/id/subcategory')
+//     .get(getSubcategories)
+//     .post(authjwt, createNewSubcategory);
 
 
 
+function getCategories(req, res, next){
+    retrieveAllCategories().then(result => res.json({result: result}))
+    .catch(error => next(error));
+}
+
+function createnewCategory (req, res, next){
+    const { body, userId } = req;
+    createCategory(body, userId).then(result => res.status(201).json({message: "Category has been added.", result: result}))
+    .catch(error => next(error));
+}
+
+// function getSubcategories(req, res, next){
+
+// }
+
+// function createNewSubcategory(req, res, next){
+    
+// }
 
 
 module.exports = router;
--- services/category.js
+++ services/category.js
@@ -1,20 +1,40 @@
 const Category = require('../models/catogry');
+const User = require('../models/user');
+//const Subcategory = require('../models/subcategory');
+const customError = require('../functions/errorHandler');
 
+
+const loggedUserCheck = async (userId) => {
+    const loggedUser = await User.findById(userId);
+    if(!loggedUser) customError("UNAUTHORIZED", 401);
+    if(loggedUser.role !== "admin") customError("UNAUTHORIZED", 401);
+}
 
 const retrieveAllCategories = async () => {
     try{
         return await Category.find();
     } catch(error) {
-        console.log(error);
-        return error;
+        return customError(error.toString(), 500);
     }
 }
 
-const createCategory = async () => {
+const createCategory = async (category, userId) => {
+    await loggedUserCheck (userId);
 
+    try{
+        const newCategory = new Category(category);
+        await newCategory.save();
+        return newCategory; 
+    } catch(error){
+        return customError(error.toString(), 500);
+    }
 }
+
+
 
 
 module.exports = {
     retrieveAllCategories,
+    createCategory,
+    //createSubcategory
 }
