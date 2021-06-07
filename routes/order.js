e2d3b06d5d342db75bf86e7ffcf24defb4ef5010
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-06-07 18:32:03 +0200

    feat: retrieve all orders

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { getOrders } = require('../services/order');
+
+router.get('/', retrieveOrders);
 
 
-
+/* Routes Handlers */
+function retrieveOrders(req, res, next){
+    getOrders().then(result => res.json({ result: result }))
+    .catch(error => next(error));
+}
 
 
 module.exports = router;
