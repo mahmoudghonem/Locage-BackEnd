81dde50458ac3b790e42ebef4be226e2a983e499
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-06-08 01:34:45 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,11 @@ module.exports = {
     script: "./app.js",
     instances: "max",
     exec_mode : "cluster",
+    env: {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+    env_production: {
+      NODE_ENV: process.env.NODE_ENV,
+    }
   }]
 };
