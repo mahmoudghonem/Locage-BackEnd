2f84a08ad71e1986cfa0ef6cd167aec39fb88b4a
Mahmoud Ghonem <mahmoudghonem28@gmail.com>
2021-07-03 15:23:48 +0200

    refactor

--- services/cart.js
+++ services/cart.js
@@ -61,7 +61,7 @@ const cartDetail = async (req, res) => {
 
             for (var c of cartItem) {
                 if (i._id.equals(c.productId)) {
-                    i.discountPrice = c.price;
+                    i.price = c.price;
                     i.quantity = c.quantity;
                 }
 
